9c65e70ac7e625525b85cb83c61524c6fd99ceba
vaskou <vaskou@hotmail.com>
2013-08-22 15:46:07 +0300

    Update 22/08/2013 15:46

--- app/addons/cs_banners/schemas/block_manager/blocks.post.php
+++ app/addons/cs_banners/schemas/block_manager/blocks.post.php
@@ -400,9 +400,447 @@ $schema['cs_banners'] = array (
 					'default_value'=>0
 				),
 			),
-        )
-    ),
+        ),
+	),
     'wrappers' => 'blocks/wrappers',
+);
+
+
+$schema['cs_banners_products'] = array (
+	'content' => array (
+		'items' => array (
+			'type' => 'enum',
+			'object' => 'products',
+			'items_function' => 'fn_get_products',
+			'remove_indent' => true,
+			'hide_label' => true,
+			'fillings' => array (
+				'manually' => array (
+					'picker' => 'pickers/products/picker.tpl',
+					'picker_params' => array (
+						'type' => 'links',
+					),
+				),
+				'newest' => array (
+					'params' => array (
+						'sort_by' => 'timestamp',
+						'sort_order' => 'desc',
+						'request' => array (
+							'cid' => '%CATEGORY_ID%'
+						)
+					)
+				),
+				'recent_products' => array (
+					'params' => array (
+						'apply_limit' => true,
+						'session' => array (
+							'pid' => '%RECENTLY_VIEWED_PRODUCTS%'
+						),
+						'request' => array (
+							'exclude_pid' => '%PRODUCT_ID%'
+						),
+						'force_get_by_ids' => true,
+					),
+					'disable_cache' => true,
+				),
+				'most_popular' => array (
+					'params' => array (
+						'popularity_from' => 1,
+						'sort_by' => 'popularity',
+						'sort_order' => 'desc',
+						'request' => array (
+							'cid' => '%CATEGORY_ID'
+						)
+					),
+				),
+			),
+		),
+	),
+	'templates'=>array(
+		'addons/cs_banners/blocks/myscroller.tpl' => array (
+			'settings' => array(
+				'csp_generalSeperator'=>array(
+					
+				),
+				'csp_mode' => array (
+					'type' => 'selectbox',
+                    'values' => array (
+                        'horizontal' => 'Horizontal',
+                        'vertical' => 'Vertical',
+                        'fade' => 'Fade',
+                    ),
+                    'default_value' => 'horizontal'
+                ),
+                'csp_speed' => array (
+                    'type' => 'input',
+                    'default_value' => 500
+                ),
+				'csp_slideMargin'=>array(
+					'type'=>'input',
+					'default_value'=>0
+				),
+				'csp_startSlide'=>array(
+					'type'=>'input',
+					'default_value'=>0
+				),
+				'csp_randomStart'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_infiniteLoop'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'true'
+				),
+				'csp_hideControlOnEnd'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				/*'csp_useCSS'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'CSS Transitions',
+						'false'=>'JS Transitions'
+					),
+					'default_value'=>'true'
+				),*/
+				'csp_easing'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'linear'=>'Linear',
+						'ease'=>'Ease',
+						'ease-in'=>'Ease-In',
+						'ease-out'=>'Ease-Out',
+						'ease-in-out'=>'Ease-In-Out',
+						
+						'swing'=>'JS Swing',
+						'easeInQuad'=>'JS Ease-In Quad',
+						'easeOutQuad'=>'JS Ease-Out Quad',
+						'easeInOutQuad'=>'JS Ease-In-Out Quad',
+						'easeInCubic'=>'JS Ease-In Cubic',
+						'easeOutCubic'=>'JS Ease-Out Cubic',
+						'easeInOutCubic'=>'JS Ease-In-Out Cubic',
+						'easeInQuart'=>'JS Ease-In Quart',
+						'easeOutQuart'=>'JS Ease-Out Quart',
+						'easeInOutQuart'=>'JS Ease-In-Out Quart',
+						'easeInQuint'=>'JS Ease-In Quint',
+						'easeOutQuint'=>'JS Ease-Out Quint',
+						'easeInOutQuint'=>'JS Ease-In-Out Quint',
+						'easeInSine'=>'JS Ease-In Sine',
+						'easeOutSine'=>'JS Ease-Out Sine',
+						'easeInOutSine'=>'JS Ease-In-Out Sine',
+						'easeInExpo'=>'JS Ease-In Expo',
+						'easeOutExpo'=>'JS Ease-Out Expo',
+						'easeInOutExpo'=>'JS Ease-In-Out Expo',
+						'easeInCirc'=>'JS Ease-In Circ',
+						'easeOutCirc'=>'JS Ease-Out Circ',
+						'easeInOutCirc'=>'JS Ease-In-Out Circ',
+						'easeInElastic'=>'JS Ease-In Elastic',
+						'easeOutElastic'=>'JS Ease-Out Elastic',
+						'easeInOutElastic'=>'JS Ease-In-Out Elastic',
+						'easeInBack'=>'JS Ease-In Back',
+						'easeOutBack'=>'JS Ease-Out Back',
+						'easeInOutBack'=>'JS Ease-In-Out Back',
+						'easeInBounce'=>'JS Ease-In Bounce',
+						'easeOutBounce'=>'JS Ease-Out Bounce',
+						'easeInOutBounce'=>'JS Ease-In-Out Bounce'						
+					),
+					'default_value'=>'linear'	
+				),
+				'csp_captions'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_ticker'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_tickerHover'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_adaptiveHeight'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_adaptiveHeightSpeed'=>array(
+					'type'=>'input',
+					'default_value'=>500
+				),
+				'csp_video'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_responsive'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'true'
+				),
+				'csp_preloadImages'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'all'=>'All',
+						'visible'=>'Visible'
+					),
+					'default_value'=>'visible'
+				),
+				'csp_touchEnabled'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'true'
+				),
+				'csp_swipeThreshold'=>array(
+					'type'=>'input',
+					'default_value'=>50
+				),
+				'csp_oneToOneTouch'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'true'
+				),
+				'csp_preventDefaultSwipeX'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'true'
+				),
+				'csp_preventDefaultSwipeY'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				
+/***********************PAGER*************************************/				
+
+				'csp_pagerSeperator'=>array(
+				),				
+				'csp_pager'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'true'
+				),
+				'csp_pagerType'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'full'=>'Full',
+						'short'=>'Short'
+					),
+					'default_value'=>'full'
+				),
+				'csp_pagerShortSeparator'=>array(
+					'type'=>'input',
+					'default_value'=>'/'
+				),
+				'csp_pagerThumbs'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				
+/**********************CONTROLS********************************/
+				'csp_controlsSeparator'=>array(
+				),				
+				'csp_controls'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'true'
+				),
+				'csp_customControls'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_nextText'=>array(
+					'type'=>'input',
+					'default_value'=>'Next'
+				),
+				'csp_prevText'=>array(
+					'type'=>'input',
+					'default_value'=>'Previous'
+				),
+				'csp_autoControls'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_autoControlsCombine'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_customAutoControls'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_startText'=>array(
+					'type'=>'input',
+					'default_value'=>'Start'
+				),
+				'csp_stopText'=>array(
+					'type'=>'input',
+					'default_value'=>'Stop'
+				),
+				
+/***********************AUTO********************************/
+				'csp_autoSeperator'=>array(
+				),
+				'csp_auto'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_pause'=>array(
+					'type'=>'input',
+					'default_value'=>4000
+				),
+				'csp_autoStart'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'true'
+				),
+				'csp_autoDirection'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'next'=>'Next',
+						'prev'=>'Previous'
+					),
+					'default_value'=>'next'
+				),
+				'csp_autoHover'=>array(
+					'type'=>'selectbox',
+					'values'=>array(
+						'true'=>'Enabled',
+						'false'=>'Disabled'
+					),
+					'default_value'=>'false'
+				),
+				'csp_autoDelay'=>array(
+					'type'=>'input',
+					'default_value'=>0
+				),
+
+/**********************CAROUSEL***************************************/
+				'csp_carouselSeperator'=>array(
+				),
+				'csp_minSlides'=>array(
+					'type'=>'input',
+					'default_value'=>1
+				),
+				'csp_maxSlides'=>array(
+					'type'=>'input',
+					'default_value'=>1
+				),
+				'csp_moveSlides'=>array(
+					'type'=>'input',
+					'default_value'=>0
+				),
+				'csp_slideWidth'=>array(
+					'type'=>'input',
+					'default_value'=>0
+				),
+				
+			),
+			'bulk_modifier' => array (
+				'fn_gather_additional_products_data' => array (
+					'products' => '#this',
+					'params' => array (
+						'get_icon' => true,
+						'get_detailed' => true,
+						'get_options' => true,
+					),
+				),
+			),
+		),
+	),
+	'settings' => array(
+		'hide_add_to_cart_button' => array (
+			'type' => 'checkbox',
+			'default_value' => 'Y'
+		),
+		'show_price' => array (
+			'type' => 'checkbox',
+			'default_value' => 'Y'
+		),
+		'enable_quick_view' => array (
+			'type' => 'checkbox',
+			'default_value' => 'N'
+		),
+	),
+	'wrappers' => 'blocks/wrappers',
+
 );
 
 return $schema;
---
+++ design/themes/basic/templates/addons/cs_banners/blocks/myscroller.tpl
@@ -0,0 +1,155 @@
+{** block-description:myscroller **}
+
+<style type="text/css">
+	
+	#bx-pager_{$block.snapping_id}
+	{
+		text-align:center;
+	}
+	
+	#bx-pager_{$block.snapping_id} a
+	{
+		margin:0 3px;
+	}
+	
+	#bx-pager_{$block.snapping_id} a > div
+	{
+		padding:3px;
+		border:solid #ccc 1px;
+		width:80px;
+		height:80px;
+		overflow:hidden;
+		display:inline-block;
+	}
+	
+	#bx-pager_{$block.snapping_id} a.active > div
+	{
+		border:solid #5280DD 1px;
+	}
+	
+	#bx-pager_{$block.snapping_id} a > div img
+	{
+		width:auto;
+		height:100%;
+	}
+	
+</style>
+
+<ul id="bxslider_{$block.snapping_id}">
+	{foreach from=$items item="product" name="for_products"}
+            <li>
+            {assign var="obj_id" value="scr_`$block.block_id`000`$product.product_id`"}
+            {if $block.properties.csp_slideWidth>='0'}
+            	{include file="common/image.tpl" assign="object_img" image_width=$block.properties.csp_slideWidth images=$product.main_pair no_ids=true}
+            {else}
+            	{include file="common/image.tpl" assign="object_img" images=$product.main_pair no_ids=true}
+            {/if}
+            <div class="slide">
+                <div>
+                    <a href="{"products.view?product_id=`$product.product_id`"|fn_url}">{$object_img nofilter}</a>
+                    {if $block.properties.enable_quick_view == "Y"}
+                        {include file="views/products/components/quick_view_link.tpl" quick_nav_ids=$quick_nav_ids}
+                    {/if}
+                </div>
+                 <div class="center compact">
+                    {if $block.properties.hide_add_to_cart_button == "Y"}
+                        {assign var="_show_add_to_cart" value=false}
+                    {else}
+                        {assign var="_show_add_to_cart" value=true}
+                    {/if}
+                    {if $block.properties.show_price == "Y"}
+                        {assign var="_hide_price" value=false}
+                    {else}
+                        {assign var="_hide_price" value=true}
+                    {/if}
+                    {strip}
+                    {include file="blocks/list_templates/simple_list.tpl" product=$product show_trunc_name=true show_price=true show_add_to_cart=$_show_add_to_cart but_role="action" hide_price=$_hide_price hide_qty=true}
+                    {/strip}
+                </div>
+            </div>
+            </li>
+        {/foreach}
+</ul>
+
+{if $block.properties.csp_customAutoControls=='true'}
+    <div id="autoCtrl"></div>
+    {assign var="csp_autoControlsSelector" value="#autoCtrl"}
+{/if}
+
+{if $block.properties.csp_customControls=='true'}
+    <div>
+        <p><span id="slider-prev"></span> | <span id="slider-next"></span></p>
+    </div>
+    {assign var="csp_nextSelector" value="#slider-next"}
+    {assign var="csp_prevSelector" value="#slider-prev"}
+{/if}
+
+{assign var="csp_easings" value=['linear','ease','ease-in','ease-out','ease-in-out']}
+{$csp_useCSS='false'}
+{foreach from=$csp_easings item=easing}
+    {if $easing==$block.properties.csp_easing}
+        {$csp_useCSS='true'}
+    {/if}
+{/foreach}
+
+{if $csp_useCSS=="false"}
+	<script src="design/themes/basic/templates/addons/cs_banners/bxslider/plugins/jquery.easing.1.3.js"></script>
+{/if}
+{if $block.properties.csp_video=='true'}
+	<script src="design/themes/basic/templates/addons/cs_banners/bxslider/plugins/jquery.fitvids.js"></script>
+{/if}
+
+<script type="text/javascript">
+$(document).ready(function(){
+	$('#bxslider_{$block.snapping_id}').bxSlider({
+		mode:'{$block.properties.csp_mode}',
+		speed:{$block.properties.csp_speed},
+		slideMargin:{$block.properties.csp_slideMargin},
+		startSlide:{$block.properties.csp_startSlide},
+		randomStart:{$block.properties.csp_randomStart},
+		infiniteLoop:{$block.properties.csp_infiniteLoop},
+		hideControlOnEnd:{$block.properties.csp_hideControlOnEnd},
+		easing:'{$block.properties.csp_easing}',
+		useCSS:{$csp_useCSS},
+		captions:{$block.properties.csp_captions},
+		ticker:{$block.properties.csp_ticker},
+		tickerHover:{$block.properties.csp_tickerHover},
+		adaptiveHeight:{$block.properties.csp_adaptiveHeight},
+		adaptiveHeightSpeed:{$block.properties.csp_adaptiveHeightSpeed},
+		video:{$block.properties.csp_video},
+		responsive:{$block.properties.csp_responsive},
+		preloadImages:'{$block.properties.csp_preloadImages}',
+		touchEnabled:{$block.properties.csp_touchEnabled},
+		swipeThreshold:{$block.properties.csp_swipeThreshold},
+		oneToOneTouch:{$block.properties.csp_oneToOneTouch},
+		preventDefaultSwipeX:{$block.properties.csp_preventDefaultSwipeX},
+		preventDefaultSwipeY:{$block.properties.csp_preventDefaultSwipeY},
+		pager:{$block.properties.csp_pager},
+		pagerType:'{$block.properties.csp_pagerType}',
+		pagerShortSeparator:'{$block.properties.csp_pagerShortSeparator}',
+		pagerCustom:'{$csp_pagerCustom}',
+		
+		controls:{$block.properties.csp_controls},
+		nextText:'{$block.properties.csp_nextText}',
+		prevText:'{$block.properties.csp_prevText}',
+		nextSelector:'{$csp_nextSelector}',
+		prevSelector:'{$csp_prevSelector}',
+		autoControls:{$block.properties.csp_autoControls},
+		autoControlsCombine:{$block.properties.csp_autoControlsCombine},
+		startText:'{$block.properties.csp_startText}',
+		stopText:'{$block.properties.csp_stopText}',
+		autoControlsSelector:'{$csp_autoControlsSelector}',
+		
+		auto:{$block.properties.csp_auto},
+		pause:{$block.properties.csp_pause},
+		autoStart:{$block.properties.csp_autoStart},
+		autoDirection:'{$block.properties.csp_autoDirection}',
+		autoHover:{$block.properties.csp_autoHover},
+		autoDelay:{$block.properties.csp_autoDelay},
+		minSlides:{$block.properties.csp_minSlides},
+		maxSlides:{$block.properties.csp_maxSlides},
+		moveSlides:{$block.properties.csp_moveSlides},
+		slideWidth:{$block.properties.csp_slideWidth},		
+  });
+});
+</script>
